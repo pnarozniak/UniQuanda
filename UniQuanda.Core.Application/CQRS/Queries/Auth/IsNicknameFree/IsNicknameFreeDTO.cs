62b4bc373a83ddb32c303684fac6430c37e3d024
xlegodroit <xxvege@gmail.com>
2022-06-18 00:16:03 +0200

    Added IsNicknameFree Query

---
+++ UniQuanda.Core.Application/CQRS/Queries/Auth/IsNicknameFree/IsNicknameFreeDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniQuanda.Core.Application.CQRS.Queries.Auth.IsNicknameFree
+{
+    public class IsNicknameFreeRequestDTO
+    {
+        [Required]
+        [MinLength(6)]
+        [MaxLength(32)]
+        public string Nickname { get; set; }
+    }
+}
---
+++ UniQuanda.Core.Application/CQRS/Queries/Auth/IsNicknameFree/IsNicknameFreeQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace UniQuanda.Core.Application.CQRS.Queries.Auth.IsNicknameFree
+{
+    public class IsNicknameFreeQuery : IRequest<bool>
+    {
+        public string Nickname { get; set; }
+        public IsNicknameFreeQuery(IsNicknameFreeRequestDTO request)
+        {
+            Nickname = request.Nickname;
+        }
+    }
+}
