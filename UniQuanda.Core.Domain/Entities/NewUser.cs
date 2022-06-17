dc41f224dd552beea36c88da91ed1be9f868a3b8
xlegodroit <xxvege@gmail.com>
2022-06-17 16:54:23 +0200

    Added domain entities and value objects

---
+++ UniQuanda.Core.Domain/Entities/NewUser.cs
@@ -0,0 +1,14 @@
+﻿using UniQuanda.Core.Domain.ValueObjects;
+
+namespace UniQuanda.Core.Domain.Entities
+{
+    public class NewUser
+    {
+        public string Nickname { get; set; }
+        public string HashedPassword { get; set; }
+        public UserOptionalInfo OptionalInfo { get; set; }
+        public string Email { get; set; }
+        public string EmailConfirmationToken { get; set; }
+        public DateTime ExistsTo { get; set; }
+    }
+}
---
+++ UniQuanda.Core.Domain/ValueObjects/UserOptionalInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace UniQuanda.Core.Domain.ValueObjects
+{
+    public class UserOptionalInfo
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? City { get; set; }
+    }
+}
