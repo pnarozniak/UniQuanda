958c4c5cb7067057151e3546a7782d802728cb23
xlegodroit <xxvege@gmail.com>
2022-06-18 00:07:37 +0200

    Added EmailService via SendGrid

--- UniQuanda.Infrastructure/Extensions.cs
+++ UniQuanda.Infrastructure/Extensions.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using UniQuanda.Core.Application.Repositories;
+using UniQuanda.Core.Application.Services;
+using UniQuanda.Core.Application.Services.Auth;
+using UniQuanda.Infrastructure.Options;
 using UniQuanda.Infrastructure.Repositories;
+using UniQuanda.Infrastructure.Services;
+using UniQuanda.Infrastructure.Services.Auth;
 
 namespace UniQuanda.Infrastructure
 {
     public static class Extensions
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
-            => services.AddScoped<IQuestionRepository, QuestionRepository>();
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Options
+            services.AddSingleton(new SendGridOptions(configuration));
+
+            // Repositories
+            services.AddScoped<IQuestionRepository, QuestionRepository>();
+            services.AddScoped<IAuthRepository, AuthRepository>();
+            
+            // Services
+            services.AddScoped<IPasswordsService, PasswordsService>();
+            services.AddScoped<ITokensService, TokensService>();
+            services.AddScoped<IEmailService, SendGridService>();
+
+            return services;
+        }
     }
 }
