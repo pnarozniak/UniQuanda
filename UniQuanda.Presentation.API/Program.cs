ce3f07d1bdf9d6b00ddfb9a8485d92b5c5de877a
xlegodroit <xxvege@gmail.com>
2022-06-17 16:49:54 +0200

    Configured DI and appsettings

--- UniQuanda.Presentation.API/Program.cs
+++ UniQuanda.Presentation.API/Program.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using UniQuanda.Infrastructure;
+using UniQuanda.Infrastructure.Presistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure();
+builder.Services.AddInfrastructurePersistence(builder.Configuration);
 builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
