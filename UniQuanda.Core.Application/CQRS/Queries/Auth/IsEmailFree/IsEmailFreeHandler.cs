e9ad5e512fcfe545313f7f7be118745531622bd5
xlegodroit <xxvege@gmail.com>
2022-06-18 00:17:13 +0200

    Added IsEmailFree Query

---
+++ UniQuanda.Core.Application/CQRS/Queries/Auth/IsEmailFree/IsEmailFreeHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using UniQuanda.Core.Application.Repositories;
+
+namespace UniQuanda.Core.Application.CQRS.Queries.Auth.IsEmailFree
+{
+    public class IsEmailFreeHandler : IRequestHandler<IsEmailFreeQuery, bool>
+    {
+        private readonly IAuthRepository _authRepository;
+        public IsEmailFreeHandler(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+        
+        public async Task<bool> Handle(IsEmailFreeQuery request, CancellationToken cancellationToken)
+        {
+            return !await _authRepository.IsEmailUsedAsync(request.Email);
+        }
+    }
+}
