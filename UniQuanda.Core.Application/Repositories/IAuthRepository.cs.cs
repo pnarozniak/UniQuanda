632656cdea243dee2e2275ce0175d6da76f1ffd2
xlegodroit <xxvege@gmail.com>
2022-06-17 17:02:24 +0200

    Added auth repository

---
+++ UniQuanda.Core.Application/Repositories/IAuthRepository.cs.cs
@@ -0,0 +1,28 @@
+﻿using UniQuanda.Core.Domain.Entities;
+
+namespace UniQuanda.Core.Application.Repositories
+{
+    public interface IAuthRepository
+    {
+        /// <summary>
+        /// Checks if given e-mail is currently being used by any user
+        /// </summary>
+        /// <param name="email">E-mail to check</param>
+        /// <returns>True if e-mail is already in use, otherwise False</returns>
+        Task<bool> IsEmailUsedAsync(string email);
+
+        /// <summary>
+        /// Checks if given nickname is currently being used by any user
+        /// </summary>
+        /// <param name="nickname">Nickname to check</param>
+        /// <returns>True if nickname is already in use, otherwise False</returns>
+        Task<bool> IsNicknameUsedAsync(string nickname);
+
+        /// <summary>
+        /// Adds new user to database
+        /// </summary>
+        /// <param name="newUser">User to add</param>
+        /// <returns>True if user has been successfully added to db, otherwise False</returns>
+        Task<bool> RegisterNewUserAsync(NewUser newUser);
+    }
+}
---
+++ UniQuanda.Infrastructure/Repositories/AuthRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniQuanda.Core.Application.Repositories;
+using UniQuanda.Core.Domain.Entities;
+using UniQuanda.Infrastructure.Presistence.AuthDb;
+using UniQuanda.Infrastructure.Presistence.AuthDb.Models;
+
+namespace UniQuanda.Infrastructure.Repositories
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly AuthDbContext _authContext;
+        public AuthRepository(AuthDbContext authContext)
+        {
+            _authContext = authContext;
+        }
+        public async Task<bool> IsEmailUsedAsync(string email)
+        {
+            return await _authContext.Users
+                .AnyAsync(u => 
+                    EF.Functions.ILike(u.IdTempUserNavigation.Email, email) ||
+                    u.Emails.Any(ue => EF.Functions.ILike(ue.Value, email)));
+        }
+
+        public async Task<bool> IsNicknameUsedAsync(string nickname)
+        {
+            return await _authContext.Users
+                .AnyAsync(u => EF.Functions.ILike(u.Nickname, nickname));
+        }
+
+        public async Task<bool> RegisterNewUserAsync(NewUser newUser)
+        {
+            var userToRegister = new User
+            {
+                Nickname = newUser.Nickname,
+                HashedPassword = newUser.HashedPassword,
+                IdTempUserNavigation = new TempUser()
+                {
+                    Email = newUser.Email,
+                    EmailConfirmationCode = newUser.EmailConfirmationToken,
+                    Birthdate = newUser.OptionalInfo.Birthdate,
+                    FirstName = newUser.OptionalInfo.FirstName,
+                    LastName = newUser.OptionalInfo.LastName,
+                    PhoneNumber = newUser.OptionalInfo.PhoneNumber,
+                    City = newUser.OptionalInfo.City,
+                    ExistsTo = newUser.ExistsTo
+                }
+            };
+
+            try
+            {
+                await _authContext.Users.AddAsync(userToRegister);
+                return await _authContext.SaveChangesAsync() >= 2;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
