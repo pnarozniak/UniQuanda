c6aa29117872d842b9002dde3e49d3c1a0885a6b
xlegodroit <xxvege@gmail.com>
2022-06-18 14:40:00 +0200

    Added DataAnnotations to registerRequestDTO

--- UniQuanda.Core.Application/CQRS/Commands/Auth/Register/RegisterDTO.cs
+++ UniQuanda.Core.Application/CQRS/Commands/Auth/Register/RegisterDTO.cs
@@ -5,20 +5,33 @@ namespace UniQuanda.Core.Application.CQRS.Commands.Auth.Register
     public class RegisterRequestDTO
     {
         [Required]
+        [MinLength(6)]
+        [MaxLength(30)]
         public string Nickname { get; set; }
         
         [Required]
+        [RegularExpression("^.*[A-Z]+[a-z]+[0-9]+.*")]
+        [MinLength(8)]
+        [MaxLength(30)]
         public string Password { get; set; }
 
         [Required]
         [MaxLength(320)]
         [EmailAddress]
         public string Email { get; set; }
-        
+
+        [MaxLength(30)]
         public string? FirstName { get; set; }
+
+        [MaxLength(30)]
         public string? LastName { get; set; }
+
         public DateTime? Birthdate { get; set; }
+
+        [MaxLength(9)]
         public string? PhoneNumber { get; set; }
+
+        [MaxLength(30)]
         public string? City { get; set; }
     }
 }
