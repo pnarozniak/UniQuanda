bd430bb80bc117c0f47e696a64632e164c5f9481
Dmitry "Ventan" Teplov <56700157+Ventan00@users.noreply.github.com>
2022-08-16 21:20:12 +0200

    Revert "Feature/QUAN-85"

---
+++ UniQuanda.Core.Application/CQRS/Queries/Question/GetQuestions/GetQuestionsDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace UniQuanda.Core.Application.CQRS.Queries.Question.GetQuestions
+{
+    public class GetQuestionsResponseDTO
+    {
+        public int QuestionId { get; set; }
+        public string Title { get; set; }
+    }
+}
