28ec75407926d3fca568905c7770aa58c1890837
Dmitry Teplov <DmitryTeplov00@gmail.com>
2022-10-29 02:18:54 +0200

    Dodano dokumentację endpointa

--- UniQuanda.Presentation.API/Controllers/TagsController.cs
+++ UniQuanda.Presentation.API/Controllers/TagsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
+using UniQuanda.Core.Application.CQRS.Queries.Auth.IsEmailAndNicknameAvailable;
 using UniQuanda.Core.Application.CQRS.Queries.Tags.GetTags;
 
 namespace UniQuanda.Presentation.API.Controllers
@@ -17,7 +18,16 @@ namespace UniQuanda.Presentation.API.Controllers
         {
             _mediator = mediator;
         }
-
+        /// <summary>
+        ///    Returns tags with filtering
+        /// </summary>
+        /// <param name="page">Page number</param>
+        /// <param name="pageSize">Amount of tags on page</param>
+        /// <param name="idTag">Parent tag Id</param>
+        /// <param name="keyword">Text to search</param>
+        /// <param name="ct">Cancellation Token</param>
+        /// <returns>Max. pageSize tags that mached conditions. When page is = 1, response will contain total tags count </returns>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetTagsResponseDTO))]
         [HttpGet]
         public async Task<IActionResult> GetTags(
             [FromQuery][Required] int page,
