2f11b9a52733d7cf5008464318435e39cbef5806
xlegodroit <xxvege@gmail.com>
2022-11-04 22:49:54 +0100

    Added tests

---
+++ UniQuanda.Tests/CQRS/Commands/Report/CreateReportHandlerTests.cs
@@ -0,0 +1,53 @@
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using UniQuanda.Core.Application.CQRS.Commands.Report.CreateReport;
+using UniQuanda.Core.Application.Repositories;
+
+namespace UniQuanda.Tests.CQRS.Commands.Report
+{
+    [TestFixture]
+    public class CreateReportHandlerTests
+    {
+        [SetUp]
+        public void SetupTests()
+        {
+            this.reportRepository = new Mock<IReportRepository>();
+            this.createReportHandler = new CreateReportHandler(this.reportRepository.Object);
+            this.createReportCommand = new CreateReportCommand(new()
+                {
+                    ReportedEntityId = 1,
+                    ReportTypeId = 2,
+                    Description = "",
+                }, 1);
+        }
+        
+        private CreateReportCommand createReportCommand;
+        private CreateReportHandler createReportHandler;
+        private Mock<IReportRepository> reportRepository;
+        
+        [Test]
+        public async Task CreateReport_ShouldReturnTrue_WhenReportIsCreated()
+        {
+            reportRepository
+                .Setup(rr => rr.CreateReportAsync(It.IsAny<CreateReportCommand>(), CancellationToken.None))
+                .ReturnsAsync(true);
+
+            var result = await createReportHandler.Handle(createReportCommand, CancellationToken.None);
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task CreateReport_ShouldReturnFalse_WhenReportIsNotCreated()
+        {
+            reportRepository
+                .Setup(rr => rr.CreateReportAsync(It.IsAny<CreateReportCommand>(), CancellationToken.None))
+                .ReturnsAsync(false);
+
+            var result = await createReportHandler.Handle(createReportCommand, CancellationToken.None);
+            result.Should().BeFalse();
+        }
+    }
+}
