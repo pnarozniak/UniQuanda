b72844cefb384443b6763bb55543821ab417bcc2
Patrykoss <adresikps@gmail.com>
2022-11-13 00:20:52 +0100

    Added confirm email command

---
+++ UniQuanda.Core.Application/CQRS/Commands/Auth/ConfirmEmail/ConfirmEmailCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace UniQuanda.Core.Application.CQRS.Commands.Auth.ConfirmEmail;
+
+public class ConfirmEmailCommand : IRequest<bool>
+{
+    public ConfirmEmailCommand(ConfirmEmailRequestDTO request)
+    {
+        Email = request.Email;
+        ConfirmationCode = request.ConfirmationCode;
+    }
+
+    public string Email { get; set; }
+    public string ConfirmationCode { get; set; }
+}
---
+++ UniQuanda.Core.Application/CQRS/Commands/Auth/ConfirmEmail/ConfirmEmailDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniQuanda.Core.Application.CQRS.Commands.Auth.ConfirmEmail;
+
+public class ConfirmEmailRequestDTO
+{
+    [Required]
+    [EmailAddress]
+    [MaxLength(320)]
+    public string Email { get; set; }
+
+    [Required]
+    public string ConfirmationCode { get; set; }
+}
