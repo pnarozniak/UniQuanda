9ae6a391ef1506a7cc6b37c8046f779fa97a9869
Dmitry "Ventan" Teplov <56700157+Ventan00@users.noreply.github.com>
2022-11-20 15:23:08 +0100

    Feature/quan 155/lista tagów (#21)
    * poprawka nazwy zmiennej
    
    * Dodano Endpoint do listy tagów
    
    * Oczyszczono projekt
    
    * Dodano dokumentację endpointa
    
    * Rozbudowano tagi o sortowanie i zwracanie liczności wszystkich tagów
    
    * Poprawiono zwracanie głównego tagu
    
    * Poprawki v1
    
    * poprawki v2
    
    * poprawki po merge
    
    * dodano seedowanie i usunięto swaggerowe enumy
    
    * Usunięcie duplikatu tagu
    
    * usunięte konstruktory

---
+++ UniQuanda.Core.Application/Repositories/ITagRepository.cs
@@ -0,0 +1,92 @@
+﻿using UniQuanda.Core.Domain.Entities.App;
+using UniQuanda.Core.Domain.Enums;
+
+namespace UniQuanda.Core.Application.Repositories
+{
+    public interface ITagRepository
+    {
+        /// <summary>
+        ///     Gets parent tags using paging
+        /// </summary>
+        /// <param name="take">Amount of tags to take</param>
+        /// <param name="skip">Amount of tags to skip</param>
+        /// <param name="orderDirection">How to order data</param>
+        /// <param name="ct">Cancelation token</param>
+        /// <returns>IEnumerable of parent tags</returns>
+        public Task<IEnumerable<TagEntity>> GetParentTagsAsync(int take, int skip, OrderDirectionEnum orderDirection, CancellationToken ct);
+        
+        /// <summary>
+        ///     Gets sub-tags using paging
+        /// </summary>
+        /// <param name="take">Amount of tags to take</param>
+        /// <param name="skip">Amount of tags to skip</param>
+        /// <param name="tagId">Id of parent tag</param>
+        /// <param name="orderDirection">How to order data</param>
+        /// <param name="ct">Cancelation token</param>
+        /// <returns>IEnumerable of sub-tags</returns>
+        public Task<IEnumerable<TagEntity>> GetSubTagsAsync(int take, int skip, int tagId, OrderDirectionEnum orderDirection, CancellationToken ct);
+        
+        /// <summary>
+        ///     Gets parent tags and subtags using keyword and paging
+        /// </summary>
+        /// <param name="take">Amount of tags to take</param>
+        /// <param name="skip">Amount of tags to skip</param>
+        /// <param name="keyword">Value to search in tag description and name</param>
+        /// <param name="orderDirection">How to order data</param>
+        /// <param name="ct">Cancelation token</param>
+        /// <returns>IEnumerable of all tags with keyword</returns>
+        public Task<IEnumerable<TagEntity>> GetTagsByKeywordAsync(int take, int skip, string keyword, OrderDirectionEnum orderDirection, CancellationToken ct);
+        
+        /// <summary>
+        ///     Gets subtags using keyword and paging
+        /// </summary>
+        /// <param name="take">Amount of tags to take</param>
+        /// <param name="skip">Amount of tags to skip</param>
+        /// <param name="tagId">Id of parent tag</param>
+        /// <param name="keyword">Value to search in tag description and name</param>
+        /// <param name="orderDirection">How to order data</param>
+        /// <param name="ct">Cancelation token</param>
+        /// <returns>IEnumerable of all tags with keyword</returns>
+        public Task<IEnumerable<TagEntity>> GetSubTagsByKeywordAsync(int take, int skip, string keyword, int tagId, OrderDirectionEnum orderDirection, CancellationToken ct);
+        
+        /// <summary>
+        ///     Get parent tags count
+        /// </summary>
+        /// <param name="ct">Cancelation token</param>
+        /// <returns>Amount of parent tags</returns>
+        public Task<int> GetParentTagsCountAsync(CancellationToken ct);
+        
+        /// <summary>
+        ///     Gets sub-tags count
+        /// </summary>
+        /// <param name="tagId">Id of parent tag</param>
+        /// <param name="ct">Cancelation token</param>
+        /// <returns>Amount of sub-tags</returns>
+        public Task<int> GetSubTagsCountAsync(int tagId, CancellationToken ct);
+
+        /// <summary>
+        ///     Gets tags with matching keyword count
+        /// </summary>
+        /// <param name="keyword">Value to search in tag description and name</param>
+        /// <param name="ct">Cancelation token</param>
+        /// <returns>Amount of tags with matching keyword</returns>
+        public Task<int> GetTagsByKeywordCountAsync(string keyword, CancellationToken ct);
+
+        /// <summary>
+        ///     Gets sub-tags with matching keyword count
+        /// </summary>
+        /// <param name="keyword">Value to search in tag description and name</param>
+        /// <param name="tagId">Id of parent tag</param>
+        /// <param name="ct">Cancelation token</param>
+        /// <returns>Amount of tags with matching keyword</returns>
+        public Task<int> GetSubTagsByKeywordCountAsync(string keyword, int tagId, CancellationToken ct);
+
+        /// <summary>
+        ///    Gets tag by id
+        /// </summary>
+        /// <param name="tagId">Id of tag</param>
+        /// <param name="ct"></param>
+        /// <returns>Cancelation token</returns>
+        public Task<TagEntity> GetTagByIdAsync(int tagId, CancellationToken ct);
+    }
+}
---
+++ UniQuanda.Presentation.API/Controllers/TagsController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UniQuanda.Core.Application.CQRS.Queries.Tags.GetTags;
+
+namespace UniQuanda.Presentation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class TagsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TagsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        /// <summary>
+        ///    Returns in a given order tags, using filtering and paging
+        /// </summary>
+        /// <param name="request">Request object</param>
+        /// <param name="ct">Cancellation Token</param>
+        /// <returns>Max. pageSize tags that mached conditions</returns>
+        /// <response code="200">When managed to get tags</response>
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(GetTagsResponseDTO))]
+        [HttpGet]
+        public async Task<IActionResult> GetTags(
+            [FromQuery]GetTagsRequestDTO request,
+            CancellationToken ct)
+        {
+            var query = new GetTagsQuery(request);
+            return Ok(await _mediator.Send(query, ct));
+        }
+    }
+}
