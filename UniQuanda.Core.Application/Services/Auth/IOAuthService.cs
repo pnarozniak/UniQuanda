724fff2b11db4e8f84c2ec48bacb916066afaeab
xlegodroit <xxvege@gmail.com>
2022-11-27 18:50:03 +0100

    Added docs

--- UniQuanda.Core.Application/Services/Auth/IOAuthService.cs
+++ UniQuanda.Core.Application/Services/Auth/IOAuthService.cs
@@ -4,6 +4,15 @@ namespace UniQuanda.Core.Application.Services.Auth;
 
 public interface IOAuthService
 {
+    /// <summary>
+    ///     Fetches google id token from google oauth API, based od google oauth login confirmation code
+    /// </summary>
+    /// <returns>GoogleIdToken if operation was successful, otherwise Null</returns>
     Task<GoogleIdToken?> GetGoogleIdTokenAsync(string code);
+
+    /// <summary>
+    ///     Gets uniquanda client url, which handles google oauth login responses
+    /// </summary>
+    /// <returns>Uniquanda client oauth handler url</returns>
     string GetGoogleClientHandlerUrl();
 }
